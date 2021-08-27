6c80e9b3fc6543a9c07d9bd07f7ff01f75e4bf02
SorataBaka <82799882+SorataBaka@users.noreply.github.com>
2021-08-27 18:21:19 +0700

    Cleaned up useless code and attempted fix on bot disconnection bug

--- commands/stream.js
+++ commands/stream.js
@@ -10,6 +10,9 @@ const {
   generateDependencyReport
 } = require('@discordjs/voice');
 const ytdl = require('ytdl-core')
+
+
+
 console.log(generateDependencyReport())
 const prefix = process.env.PREFIX
 
@@ -19,65 +22,69 @@ module.exports = {
   description: "Streams an audio from a LIVE youtube source",
   usage: `${prefix} stream {Youtube LIVE URL}`,
   async execute(message, client, args) {
-    var interval;
-    if(client.voice.adapters.has(message.guild.id))return message.reply("The bot is already active. Please stop it first.")
+    //Do local checks to see if player is able to use this command and execute function
     if(!args) return message.reply("Please provide a link to a youtube stream!")
     if(!ytdl.validateURL(args)) return message.reply("Invalid URL provided")
-    if(!message.member.permissions.has("ADMINISTRATOR") || !message.member.permissions.has("MANAGE_SERVER")) return message.reply("I'm sorry, you don't have permission to use this command!")
-
-    var stream = ytdl(args, {quality: "highest"})
-
-    stream.on("error", err => console.error(err))
+    if(!message.member.permissions.has("BAN_MEMBERS")) return message.reply("I'm sorry, you don't have permission to use this command!")
     if(!message.member.voice.channel) return message.reply("You are currently not in a voice channel!")
+
     const voiceChannel = message.member.voice.channel
 
+
+    //create a stream from youtube and convert it into a playable resource
+    var stream = ytdl(args, {quality: "highest"})
+    stream.on("error", err => {
+      console.error(err)
+      return message.reply("Unable to create stream.")
+    }) 
+    const resource = createAudioResource(stream)
+
+
+    //create an audio player to play the resource and play
     const player = createAudioPlayer({
       behaviors: {
-        noSubscriber: NoSubscriberBehavior.Pause,
+        noSubscriber: NoSubscriberBehavior.Play,
         maxMissedFrames: Math.round(5000 / 20)
       }
     })
-    
-    player.on('stateChange', (oldState, newState) => {
-      if (oldState.status === AudioPlayerStatus.Idle && newState.status === AudioPlayerStatus.Playing) {
-        console.log('Playing audio output on audio player');
-        message.reply("Playing audio output on audio channel")
-      } else if (newState.status === AudioPlayerStatus.Idle) {
-        console.log('Playback has stopped. Attempting to restart.');
-        message.reply("Playback stopped, attempting to restart")
-        stream.destroy()
-        stream = ytdl(args, {quality: "highest"})
-        const resource = createAudioResource(stream)
-        player.play(resource,{inputType: StreamType.OggOpus})
-      }
-      if(newState.status == "autopaused"){
-        return player.stop()
-      }
-    })
-    // {filter: "audioonly", quality: "highest", liveBuffer: 60000, highWaterMark: 1 << 25}
-    const resource = createAudioResource(stream)
     player.play(resource,{inputType: StreamType.OggOpus})
+
+    //at this state the bot is already receiving and streaming music from youtube. 
+
     const connectToChannel = async(channel) => {
       const connection = joinVoiceChannel({
         channelId: channel.id,
         guildId: channel.guild.id,
         adapterCreator: channel.guild.voiceAdapterCreator,
       })
-      client.connection = connection
-      client.player = player
-      client.interval = interval
       const ready = await entersState(connection, VoiceConnectionStatus.Ready, 30_000).catch(err => {
         console.error(err)
         return message.reply("Error joining voice channel")
       })
       if(ready) return connection
-      }
+    }
+
+    //Connect to channel and check if able to establish a connection
+    var connection = await connectToChannel(voiceChannel);
     
-    const connection = await connectToChannel(voiceChannel);
-    if(!connection) return message.reply("It seems like there has been a problem joining your channel! Please make sure it is not locked or full.")
-    const subscribe = await connection.subscribe(player)
-    console.log(client.interval)
+    const subscribe = connection.subscribe(player)
     if(subscribe) return message.reply("Now streaming music")
-    
+
+    player.on('stateChange', (oldState, newState) => {
+      if (oldState.status === AudioPlayerStatus.Idle && newState.status === AudioPlayerStatus.Playing) {
+        message.reply("Playing audio output on audio channel")
+
+      } else if (newState.status === AudioPlayerStatus.Idle) {
+        message.reply("Playback stopped, attempting to restart")
+
+        stream.destroy()
+        stream = ytdl(args, {quality: "highest"})
+        const resource = createAudioResource(stream)
+        player.play(resource,{inputType: StreamType.OggOpus})
+      }
+    })
+
+    client.connection = connection
+    client.player = player
   }
 }
